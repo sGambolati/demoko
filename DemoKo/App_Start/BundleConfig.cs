7141cde894b9318186d935f78a110cfb5a829b35
Sebastian Gambolati <sebastiang@lagash.com>
2016-04-03 19:16:44 -0300

    Bootstrap compatible & more initial stuff.

--- DemoKo/App_Start/BundleConfig.cs
+++ DemoKo/App_Start/BundleConfig.cs
@@ -20,10 +20,13 @@ namespace DemoKo
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
