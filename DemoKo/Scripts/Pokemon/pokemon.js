ed28b045089215b99287cabb06a64eb95528087e
sgambolati <sebastiang@lagash.com>
2016-04-03 20:24:31 -0300

    Add missing files.

--- DemoKo/Scripts/Pokemon/pokemon.js
+++ DemoKo/Scripts/Pokemon/pokemon.js
@@ -3,5 +3,10 @@
     this.name = ko.observable("Pikachu");
     this.number = ko.observable("25");
 
-    this.selectedType = ko.observable(0);
-};
+    this.availablesTypes = ["Acero", "Agua", "Bicho", "Dragón", "Eléctrico", "Fantasma", "Fuego", "Hada", "Hielo", "Lucha", "Normal", "Planta", "Psíquico", "Roca", "Siniestro", "Tierra", "Veneno", "Volador"];
+    this.selectedType = ko.observable("Eléctrico");
+    
+    this.send = function () {
+        alert(this.selectedType());
+    };
+}
