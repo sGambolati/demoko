6023f1cc587a7f807f8fd21dcabccb7f2469ab86
sgambolati <sebastiang@lagash.com>
2016-04-05 21:43:06 -0300

    Add image with computed & subscribe.

--- DemoKo/Scripts/Pokemon/pokemon.js
+++ DemoKo/Scripts/Pokemon/pokemon.js
@@ -7,6 +7,31 @@
     this.availablesTypes = ["Acero", "Agua", "Bicho", "Dragón", "Eléctrico", "Fantasma", "Fuego", "Hada", "Hielo", "Lucha", "Normal", "Planta", "Psíquico", "Roca", "Siniestro", "Tierra", "Veneno", "Volador"];
     this.selectedType = ko.observable("Eléctdddrico");
     
+    //Computed
+    this.imageUrl = ko.computed(function () {
+        var number = this.number();
+        if (this.number() < 10)
+            number = "0" + number;
+        if (this.number() < 100)
+            number = "0" + number;
+
+        return "http://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + number + ".png";
+    }, this);
+
+    //Subscribe
+    //this.imageUrl = ko.observable("");
+    //this.number.subscribe(function (newValue) {
+    //    //this.imageUrl("http://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + newValue + ".png");
+
+    //    var number = newValue;
+    //    if (newValue < 10)
+    //        number = "0" + number;
+    //    if (newValue < 100)
+    //        number = "0" + number;
+    //    console.log("http://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + number + ".png");
+    //}, this);
+
+
     this.send = function () {
         alert(this.selectedType());
     };
